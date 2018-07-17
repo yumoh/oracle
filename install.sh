cc44602b935c545b05ae0ca26d02b1397e5f95e3
yumohc <yumohc@163.com>
2018-07-17 17:23:02 +0800

    新建文件 install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/usr/bin/bash
+
+apt install libaio1
+mkdir -p /opt/oracle
+cd /opt/oracle
+
+unzip instantclient-basic-linux-x86-64-11.2.0.2.0.zip 
+sudo sh -c "echo /opt/oracle/instantclient_11_2 > /etc/ld.so.conf.d/oracle-instantclient.conf"
+sudo ldconfig
+export LD_LIBRARY_PATH=/opt/oracle/instantclient_11_2:$LD_LIBRARY_PATH
