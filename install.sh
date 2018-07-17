4f17f6051a9a2fd6d9ce7b63cd76e1899c2e1399
yumohc <yumohc@163.com>
2018-07-17 17:59:15 +0800

    更新文件 install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #/usr/bin/bash
 
+# wget http://yumohc.coding.me/oracle/install.sh
+
 apt install libaio1
 mkdir -p /opt/oracle
 cd /opt/oracle
-
+wget http://yumohc.coding.me/oracle/instantclient-basic-linux-x86-64-11.2.0.2.0.zip
 unzip instantclient-basic-linux-x86-64-11.2.0.2.0.zip 
 sudo sh -c "echo /opt/oracle/instantclient_11_2 > /etc/ld.so.conf.d/oracle-instantclient.conf"
 sudo ldconfig
